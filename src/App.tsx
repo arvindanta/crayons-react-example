48356a60f9bbc094e0a52464f6c9a6837f00bc0f
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-11-28 23:26:27 +0530

    toast examples added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+
+import React, { useState, useEffect, useRef} from "react";
 import {
   FwButton,
   FwDropdownButton,
@@ -8,10 +9,27 @@ import {
   FwModalTitle,
   FwSelect,
   FwSelectOption,
-} from "@freshworks/crayons/react";
+  ToastController,
+  FwToast,
+  FwToastMessage,
+} from "@freshworks/crayons-1/react";
 function App() {
+  const el = useRef<any>(null);
+  const el1 = useRef<any>(null);
+  const [val,setVal] = useState(1)
+  const toast = ToastController();
+
+  useEffect(() => {
+   el1.current.onclick = function() {
+     console.log("custom action handled here");
+     setVal(22)
+   }
+    
+  },[])
+
   return (
     <div className="App">
+      val - {val}
       <FwButton onFwClick={() => console.log("fwclicked")}>
         Default button with custom event handling
       </FwButton>
@@ -98,6 +116,60 @@ function App() {
         <FwSelectOption value="5">Tyrell</FwSelectOption>
       </FwSelect>
       <br /> <br />
+      <FwToastMessage type='success' id="custom-temp">
+            <div>
+              {/* <FwButton onclick={()=> console.log("custom action here")}>crayons button custom content</FwButton> */}
+              <FwButton ref={el1}>custom action content in Typescript apps</FwButton>
+              <p> Please make sure here in the above element, the event handler name is in lowercase. use `onclick` instead of `onClick`. </p>
+              <p className="cus-style">custom style content</p>
+            </div>
+          </FwToastMessage>
+
+      <FwToast id="type_toast" 
+      timeout={5000}></FwToast>
+      <FwButton
+        onClick={() =>
+          toast.trigger({ type: "success", content: "Successfullly triggered" })
+        }
+      >
+        Success
+      </FwButton>
+      <FwButton
+        onClick={() =>
+          el.current.trigger({
+            type: "error",
+            content: "something went wrong!",
+          })
+        }
+      >
+        Error
+      </FwButton>
+      <FwButton
+        onClick={() =>
+          el.current.trigger({ type: "warning", content: "This is a warning!" })
+        }
+      >
+        Warning
+      </FwButton>
+      <FwButton
+        onClick={() =>
+          el.current.trigger({
+            type: "inprogress",
+            content: "Request is in progress",
+          })
+        }
+      >
+        Inprogress
+      </FwButton>
+      <FwButton
+        onClick={() =>
+          toast.trigger({ contentref: "#custom-temp", sticky: true })
+        }
+      >
+        trigger custom{" "}
+      </FwButton>
+
+     
     </div>
   );
 }
