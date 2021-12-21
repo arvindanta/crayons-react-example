ddbc10414fd7adbe2f28bb870b8021ea266a7b2d
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-12-21 20:14:05 +0530

    handle multiselect

--- src/FwForm.js
+++ src/FwForm.js
@@ -226,6 +226,12 @@ function FwForm({
       //   inputType === "MULTI_SELECT"
       //     ? values[field]?.map((v) => v.value || v)
       //     : values[field]?.value || values[field] ,
+      value:
+      inputType === 'MULTI_SELECT'  // for multiselect pass Array
+        ? values[field]?.map((v) => v.value || v)
+        : Array.isArray(values[field]) // single select but the value is an array, pass 0th index
+        ? values[field]?.map((v) => v.value || v)[0]
+        : values[field],
     });
 
     const labelProps = (field, value) => ({
