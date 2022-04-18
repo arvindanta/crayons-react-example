b85be0db8a4a6c4f6c54fe92255ee5f799563280
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-04-18 20:44:30 +0530

    sample react builds

--- server1.js
+++ server1.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build1')));
 app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'build1/main.html'));
 });
 
-app.listen(9001);
+app.listen(9000);
--- server2.js
+++ server2.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'build1')));
+app.use(express.static(path.join(__dirname, 'build2')));
 app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build1/main.html'));
+  res.sendFile(path.join(__dirname, 'build2/index.html'));
 });
 
-app.listen(9000);
+app.listen(9001);
