214f96a09f8a12b692122ccdd3aaf31ff8235d79
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-04-19 17:16:57 +0530

    using mfe-controller on window object for mounting and unmounting

--- src/index.tsx
+++ src/index.tsx
@@ -2,41 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
 const APP_ID = "reactForm";
 
-declare global {
-  interface Window {
-    mount: (container: any, appProps: any) => void;
-  }
-}
-window[APP_ID as any] = window[APP_ID as any] || {};
-window[APP_ID as any].mount = async (container: any, appProps: any) => {
-  if (!container) {
-    console.warn(`APP - ${APP_ID} container not found`);
-    return;
-  }
+const rootConfig = {
+  mount: async (container: any, appProps: any) => {
+    if (!container) {
+      console.warn(`APP - ${APP_ID} container not found`);
+      return;
+    }
 
-  console.log(`MOUNT: ${APP_ID}`, container, appProps);
+    console.log(`MOUNT: ${APP_ID}`, container, appProps);
 
-  ReactDOM.render(<App {...appProps} />, container);
-
-  return async () => {
+    ReactDOM.render(<App {...appProps} />, container);
+  },
+  unmount: async (container: any) => {
     console.log(`UNMOUNT: ${APP_ID}`);
     ReactDOM.unmountComponentAtNode(container);
-  };
+  },
 };
 
-console.log("window appid ", window[APP_ID as any]);
+(window as any).MFEController?.registerApplication?.(APP_ID, rootConfig);
 
 window.onload = () => {
-  window[APP_ID as any].mount(document.getElementById("root"), {
+  rootConfig.mount(document.getElementById("root"), {
     title: "test",
   });
 };
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// )
